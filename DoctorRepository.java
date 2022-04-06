9e4867e1fc995e0ac4a27f37fee99622ed04fe2b
Dnyaneshwariraut12 <95605312+Dnyaneshwariraut12@users.noreply.github.com>
2022-04-06 15:16:10 +0530

    Update DoctorRepository.java

--- DoctorRepository.java
+++ DoctorRepository.java
@@ -7,14 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.edu.HMS.entity.Doctor;
-import com.edu.HMS.entity.Patient;
 
 public interface DoctorRepository extends JpaRepository<Doctor,Long>{
 	List<Doctor> findByFirstName(String firstName);
 
 	List<Doctor> findByLastName(String lastName);
 
-	@Query("select p from Patient p where p.firstName = :name ")//JPQL
-    List<Patient> findEmployeesByFirstName(@Param("name") String firstName);//john
+	@Query("select d from Doctor d where d.firstName = :name ")//JPQL
+    List<Doctor> findDoctorByFirstName(@Param("name") String firstName);//john
 
 }
