5e5dfbf9b1c42a5862e5a9144ff2fce42017018f
Dnyaneshwariraut12 <95605312+Dnyaneshwariraut12@users.noreply.github.com>
2022-04-06 15:17:46 +0530

    Update DoctorService.java

--- DoctorService.java
+++ DoctorService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.edu.HMS.entity.Doctor;
-import com.edu.HMS.entity.Patient;
+
 
 @Service
 public interface DoctorService{
